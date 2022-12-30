2d763843b79744ae2be2c96ae6d9991696b6de68
Vinicius999 <viniciussilva.dsc@gmail.com>
2022-12-30 19:01:22 -0300

    Add sidebar

---
+++ basketball_app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import base64
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+st.title('NBA Player Stats Explorer')
+
+st.markdown("""
+This app performs simple webscraping of NBA player stats data!
+* **Python libraries:** base64, pandas, streamlit
+* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
+""")
+
+st.sidebar.header('User Input Feature')
