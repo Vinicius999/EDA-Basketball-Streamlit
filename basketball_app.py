ee8141b428cbc43617969560994384effa8e8b46
Vinicius999 <viniciussilva.dsc@gmail.com>
2022-12-31 17:53:45 -0300

    Add drop-down menu

--- basketball_app.py
+++ basketball_app.py
@@ -14,3 +14,7 @@ This app performs simple webscraping of NBA player stats data!
 """)
 
 st.sidebar.header('User Input Feature')
+selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950, 2020))))
+
+
+
