1de8f8c6f96b4a8e81ce398ab5d263a32a0adb2f
Vinicius999 <viniciussilva.dsc@gmail.com>
2023-01-04 21:19:12 -0300

    Showing dataframe with the results

--- basketball_app.py
+++ basketball_app.py
@@ -40,4 +40,6 @@ selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
 # Filtering data
 df_selected_team = playerstats[(playerstats['Tm'].isin(selected_team)) & (playerstats['Pos'].isin(selected_pos))]
 
-
+st.header('Display Player Stats of Selected Team(s)')
+st.write(f'Data Dimension: {str(df_selected_team.shape[0])} rows and {str(df_selected_team.shape[1])} columns.')
+st.dataframe(df_selected_team)
